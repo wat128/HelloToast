f1d8aad966070d7312a00aaee0152c752a9020df
wat128 <wat128sideb@gmail.com>
2020-06-06 19:20:30 +0900

    Add onClick handlers for the buttons

--- app/src/main/java/com/wat128/hellotoast/MainActivity.java
+++ app/src/main/java/com/wat128/hellotoast/MainActivity.java
@@ -3,12 +3,32 @@ package com.wat128.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int _count = 0;
+    private TextView _showCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _showCount = findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        ++_count;
+        if(_showCount != null)
+            _showCount.setText(Integer.toString(_count));
+
     }
 }
